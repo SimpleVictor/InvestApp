8aa9b2a7fdd2f0efed9e2accf4d47639248a1173
Patrick Dowling <patrickdowling94@gmail.com>
2016-09-18 04:22:18 -0400

    Adding zillow functionality.

--- server/routes/login.ts
+++ server/routes/login.ts
@@ -1,5 +1,19 @@
 import { Router, Request, Response, NextFunction } from "express";
+var Zillow  = require('node-zillow')
 
+var parameters = {
+    address: "78 Albany Ave",
+    citystatezip: "Pompton Lakes, NJ 07442",
+    rentzestimate: true
+};
+var zwsid = "X1-ZWz1ffywarbf9n_3csw9"
+var zillow = new Zillow(zwsid)
+zillow.get('GetSearchResults', parameters)
+    .then(function(results) {
+        console.log(results['response']['results']['result'][0])
+        return results;
+        // results here is an object { message: {}, request: {}, response: {}}
+    })
 
 
 const loginRouter: Router = Router();
